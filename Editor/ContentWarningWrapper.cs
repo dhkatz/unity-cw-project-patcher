01db4e1f8a8d108a55518e35b821848f0a2c5d2e
David Katz <dhkatz.16@gmail.com>
2024-12-18 12:34:39 -0800

    Fix asset copying of scenes

--- Editor/ContentWarningWrapper.cs
+++ Editor/ContentWarningWrapper.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using Nomnom.UnityProjectPatcher.Editor;
 using Nomnom.UnityProjectPatcher.Editor.Steps;
 
@@ -6,10 +7,12 @@ namespace Dhkatz.ContentWarningProjectPatcher.Editor.Editor
     [UPPatcher("dev.dhkatz.unity-cw-project-patcher")]
     public static class ContentWarningWrapper
     {
+        [UsedImplicitly]
         public static void GetSteps(StepPipeline pipeline)
         {
             pipeline.SetInputSystem(InputSystemType.InputSystem_New);
             pipeline.IsUsingAddressables();
+            pipeline.InsertAfter<InjectURPAssetsStep>(new ChangeSceneListStep("NewMainMenu"));
             pipeline.SetGameViewResolution("16:9");
             pipeline.OpenSceneAtEnd("NewMainMenu");
         }
